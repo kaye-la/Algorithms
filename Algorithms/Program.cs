1e917386a1845324729473f1ecdbec70705aa46d
Еврейчик S < S>
2020-06-20 15:52:39 +0300

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿using Algorithms.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bubble Sorting Test
+            Console.WriteLine("\n-----------Bubble Sorting-------------");
+
+            int[] tempSort = { 1, 10, 2, 5, 3, 7 };
+            Console.Write("Array: ");
+            for (int i = 0; i < tempSort.Count(); i++)
+                Console.Write(tempSort[i] + " ");
+
+            BubbleSort<int>.Sort(tempSort);
+            Console.Write("\nSorted array: ");
+            for (int i = 0; i < tempSort.Count(); i++)
+                Console.Write(tempSort[i] + " ");
+            Console.WriteLine();
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    public class BubbleSort<T> where T: IComparable
+    {
+        public static void Sort(T[] arr)
+        {
+            T temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i].CompareTo(arr[j]) > 0)
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public void Swap(T[] arr, int a, int b)
+        {
+            T temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+    }
+}
